98b32841a689a8cf1860e52e4033c1985a9a755d
Noah091213 <noahvryens@gmail.com>
2026-03-30 20:16:17 +0200

    Finished code, now the guestimate just needs optimization

--- Workspace/MultiDimRoots.cpp
+++ Workspace/MultiDimRoots.cpp
@@ -20,7 +20,7 @@ struct vectorFunction {
         equations[2] = 2 * q[3] + 2 * k * cos(q[4]) -d;
         equations[3] = q[2] + k * sin(q[4]) -n;
         equations[4] = sinh(q[3]/q[6]) -tan(q[5]);
-        equations[5] = (1 + v/(w * q[1] * q[6])) * tan(q[5]) -tan(q[4]);
+        equations[5] = (1 + v/(w * q[0])) * tan(q[5]) -tan(q[4]);
         equations[6] = q[0] * (1 + alpha * q[7]) -q[1];
         equations[7] = (w * q[0])/(2 * sin(q[5])) -q[7];
         return equations;
@@ -30,21 +30,22 @@ struct vectorFunction {
 
 int main() {    
 
-    double v     = 120; // Insulators weight in kg
-    double k     = 2.5; // length of insulators
-    double w     = 4.0; // Weight of cable kg pr m, when resting
-    double alpha = 2*pow(10,-7);   // Elasticity coefficient of cable 
-    
-    double d;       // Distance
-    double L0;      // Resting length
-    double L;       // Suspended length of the cable
-    double n;       // Sagging of cable from the insulators attachement points
-    double p;       // Sagging of cable from the cables attachment points
-    double x;       // half the distance between the cables attachment points
-    double theta;   // Angle between insulator and horizontal plane at attachment points
-    double phi;     // Angle between cable and horizontal plane at attachment point
-    double a;       // Parameter in the catenary equation for the cable
-    double H;       // String tension in the cable
+    // Given values
+    double v     = 120;             // Insulators weight in kg
+    double k     = 2.5;             // length of insulators
+    double w     = 4.0;             // Weight of cable kg pr m, when resting
+    double alpha = 2*pow(10,-7);    // Elasticity coefficient of cable 
+    double d     = 30;              // Distance between pylons, it is given as variable, but later stated as fact    
+    // Guesstimated values
+    double a     = 40;                              // Parameter in the catenary equation for the cable
+    double H     = 100;                             // String tension in the cable
+    // Calculated values
+    double x     = (d/2.0) - k;                     // half the distance between the cables attachment points
+    double L     = 2.0*a*sinh(x/a);                 // Suspended length of the cable
+    double L0    = L;                               // Resting length
+    double p     = a*(cosh(x/a) - 1.0);             // Sagging of cable from the cables attachment points
+    double phi   = atan(sinh(x/a));                 // Angle between cable and horizontal plane at attachment point
+    double theta = atan((1.0+v/(w*L0))*tan(phi));   // Angle between insulator and horizontal plane at attachment points
 
     bool statusBool;
 
@@ -62,7 +63,7 @@ int main() {
         q[6] = a;
         q[7] = H;
 
-        vectorFunction F(v,k,w,alpha,d,n);
+        vectorFunction F(v, k, w, alpha, d, nValues[i]);
         newt(q, statusBool, F);
 
         // Safety check
@@ -70,8 +71,8 @@ int main() {
             std::cout << "Checked n = " << nValues[i] << " and a solution could not be found" << std::endl;
         } else {
             std::cout << "Checked n = " << nValues[i] << std::endl;
-            std::cout << "Cable length at resting position = " << std::endl;
-            std::cout << "String tension in cable at resting = " << std::endl; 
+            std::cout << "Cable length at resting position = " << q[0] << std::endl;
+            std::cout << "String tension in cable at resting = " << q[7] << "\n" << std::endl; 
         }
     }
 
